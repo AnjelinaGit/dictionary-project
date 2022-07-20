933e77b6a56af83b5974c6ffca0ab9f14815fef9
AnjelinaGit <101161259+AnjelinaGit@users.noreply.github.com>
2022-07-20 13:39:54 +1000

    Added Dictionary Search Engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
@@ -7,6 +8,12 @@ export default function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Anjelina Kabul</small>
+      </footer>
     </div>
   );
 }
