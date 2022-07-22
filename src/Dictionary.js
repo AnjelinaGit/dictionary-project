e13a0ad2cbdb16e09aacd9128b2ecfda4b2bc979
AnjelinaGit <101161259+AnjelinaGit@users.noreply.github.com>
2022-07-22 17:15:46 +1000

    Fixed code

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,29 +2,35 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState(" ");
+
+export default function Dictionaty() {
+  let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
-
-  function handleResponse(response) {
-    setResults(response.data[0]);
-  }
-  function search(event) {
-    event.preventDefault();
-
-    //documentation : https://api.dictionaryapi.dev/
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
-  }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+    setResults(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    // documentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
+        <input
+          type="search"
+          autoFocus={true}
+          onChange={handleKeywordChange}
+        ></input>
       </form>
       <Results results={results} />
     </div>
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
@@ -9,8 +10,7 @@ export default function Meaning(props) {
           <div key={index}>
             <p>
               {definition.definition}
-              <br />
-              <em>{definition.example} </em>
+              <em>{definition.example}</em>
             </p>
           </div>
         );
