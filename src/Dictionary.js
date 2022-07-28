f2dc72b46c581264e991d9fcf5f403bafc3ba9c6
AnjelinaGit <101161259+AnjelinaGit@users.noreply.github.com>
2022-07-28 15:44:21 +1000

    Rendered and fixed code

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,22 +8,23 @@ export default function Dictionary(props) {
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
 
-  function handleKeywordChange(event) {
-    setKeyword(event.target.value);
-  }
-
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
   function search() {
-    // documentation: https://dictionaryapi.dev/
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
+
   function handleSubmit(event) {
     event.preventDefault();
     search();
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
   }
 
   function load() {
@@ -35,18 +36,16 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section>
-          <h1>What word do you want to look</h1>
-          <form onSubmit={search}>
+          <h1>What word do you want to look up?</h1>
+          <form onSubmit={handleSubmit}>
             <input
               type="search"
-              autoFocus={true}
               onChange={handleKeywordChange}
               defaultValue={props.defaultKeyword}
-            ></input>
+            />
           </form>
-
           <div className="hint">
-            Suggested words: sunset, wine, yoga, plant...
+            suggested words: sunset, wine, yoga, plant...
           </div>
         </section>
         <Results results={results} />
